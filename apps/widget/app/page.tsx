00d6555179e4f17cfef3b73d93b2b3ad597dd09c
Naveen Addanki <addankinaveen84@gmail.com>
2025-10-15 17:56:02 -0400

    06: ai voice agent

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,18 +1,21 @@
 "use client"
 
-import { useMutation, useQuery } from "convex/react";
-import {api} from "@workspace/backend/_generated/api";
+import { useVapi } from "@/modules/widget/hooks/use-vapi";
 import { Button } from "@workspace/ui/components/button";
 
 
 export default function Page() {
-  const users = useQuery(api.users.getMany);
-  const addUser = useMutation(api.users.add);
+  const { startCall, endCall, isConnected, isConnecting, isSpeaking, transcript } = useVapi();
 
   return (
     <div className="flex items-center justify-center min-h-svh">
-        <Button onClick={() => addUser()}>Add User</Button>
-        <p>{JSON.stringify(users)}</p>
+        <Button onClick={() => startCall()}>start call</Button>
+        <Button onClick={() => endCall()}>end call</Button>
+        <p>is connected: {isConnected ? "true" : "false"}</p>
+        <p>is connecting: {isConnecting ? "true" : "false"}</p>
+        <p>is speaking: {isSpeaking ? "true" : "false"}</p>
+        <p>transcript: {JSON.stringify(transcript)}</p>
+            
     </div>
   )
 }

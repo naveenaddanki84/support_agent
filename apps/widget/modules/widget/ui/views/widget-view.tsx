c0b43f3a1627a8ee8ef5fbe03b4361e0ec86321a
Naveen Addanki <addankinaveen84@gmail.com>
2025-10-16 12:58:50 -0400

    11: widget screen router

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-//import { useAtomValue } from "jotai";
+import { useAtomValue } from "jotai";
 import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
-//import { screenAtom } from "@/modules/widget/atoms/widget-atoms";
+import { screenAtom } from "@/modules/widget/atoms/widget-atoms";
 //import { WidgetErrorScreen } from "@/modules/widget/ui/screens/widget-error-screen";
 //import { WidgetLoadingScreen } from "@/modules/widget/ui/screens/widget-loading-screen";
 //import { WidgetSelectionScreen } from "@/modules/widget/ui/screens/widget-selection-screen";
@@ -16,13 +16,23 @@ interface Props {
 };
 
 export const WidgetView = ({ organizationId }: Props) => {
-  //const screen = useAtomValue(screenAtom);
+  const screen = useAtomValue(screenAtom);
 
+  const screenComponents = {
+    loading: <p>TODO: Loading</p>,
+    error: <p>TODO: Error</p>,
+    auth: <WidgetAuthScreen />,
+    voice: <p>TODO: Voice</p>,
+    inbox: <p>TODO: Inbox</p>,
+    selection: <p>TODO: Selection</p>,
+    chat: <p>TODO: Chat</p>,
+    contact: <p>TODO: Contact</p>,
+  }
   
   return (
     // TODO: Confirm whether or not "min-h-screen" and "min-w-screen" is needed
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
+      {screenComponents[screen]}
         
       {/*<WidgetFooter />*/}
     </main>

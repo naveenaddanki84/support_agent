d136bdcc17101914550bb50f4b57223ee9b59178
Naveen Addanki <addankinaveen84@gmail.com>
2025-10-16 00:34:25 -0400

    07: dashboard-layout

---
+++ apps/web/app/(dashboard)/customization/page.tsx
@@ -0,0 +1,9 @@
+const CustomizationPage = () => {
+  return (
+    <div>
+      <h1>Customization</h1>
+    </div>
+  );
+};
+
+export default CustomizationPage;
---
+++ apps/web/app/(dashboard)/files/page.tsx
@@ -0,0 +1,9 @@
+const FilesPage = () => {
+  return (
+    <div>
+      <h1>Files</h1>
+    </div>
+  );
+};
+
+export default FilesPage;

1c90cdaa9cfb81d18eca3b2c443200b6e19bdf23
Naveen Addanki <addankinaveen84@gmail.com>
2025-10-16 19:56:17 -0400

    16: widget inbox

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -7,7 +7,7 @@ import { WidgetErrorScreen } from "@/modules/widget/ui/screens/widget-error-scre
 import { WidgetLoadingScreen } from "@/modules/widget/ui/screens/widget-loading-screen";
 import { WidgetSelectionScreen } from "@/modules/widget/ui/screens/widget-selection-screen";
 import { WidgetChatScreen } from "@/modules/widget/ui/screens/widget-chat-screen";
-//import { WidgetInboxScreen } from "../screens/widget-inbox-screen";
+import { WidgetInboxScreen } from "../screens/widget-inbox-screen";
 import {WidgetFooter} from "../components/widget-footer";
 import {WidgetHeader} from "../components/widget-header";
 
@@ -23,7 +23,7 @@ export const WidgetView = ({ organizationId }: Props) => {
     error: <WidgetErrorScreen />,
     auth: <WidgetAuthScreen />,
     voice: <p>TODO: Voice</p>,
-    inbox: <p>TODO: Inbox</p>,
+    inbox: <WidgetInboxScreen />,
     selection: <WidgetSelectionScreen />,
     chat: <WidgetChatScreen />,
     contact: <p>TODO: Contact</p>,

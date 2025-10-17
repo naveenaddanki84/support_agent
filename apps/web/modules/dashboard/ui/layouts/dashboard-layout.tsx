45a73dbab0dd725543ed54171f8f68c833523962
Naveen Addanki <addankinaveen84@gmail.com>
2025-10-17 00:37:21 -0400

    17,18,19 : dashboard+ai tools

--- apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -2,7 +2,7 @@ import { AuthGuard } from "@/modules/auth/ui/components/auth-guard"
 import { OrganizationGuard } from "@/modules/auth/ui/components/organization-guard"
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
 import { SidebarProvider } from "@workspace/ui/components/sidebar";
-//import { Provider } from "jotai";
+import { Provider } from "jotai";
 import { cookies } from "next/headers";
 
 export const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
@@ -13,14 +13,14 @@ export const DashboardLayout = async ({ children }: { children: React.ReactNode 
   return (
     <AuthGuard>
       <OrganizationGuard>
-        {/*<Provider>*/}
+        <Provider>
           <SidebarProvider defaultOpen={defaultOpen}>
             <DashboardSidebar />
             <main className="flex flex-1 flex-col">
               {children}
             </main>
           </SidebarProvider>
-        {/*</Provider>*/}
+        </Provider>
       </OrganizationGuard>
     </AuthGuard>
   );
--- packages/backend/convex/public/messages.ts
+++ packages/backend/convex/public/messages.ts
@@ -3,8 +3,8 @@ import { action, query } from "../_generated/server";
 import { components, internal } from "../_generated/api";
 import { supportAgent } from "../system/ai/agents/supportAgent";
 import { paginationOptsValidator } from "convex/server";
-//import { escalateConversation } from "../system/ai/tools/escalateConversation";
-//import { resolveConversation } from "../system/ai/tools/resolveConversation";
+import { escalateConversation } from "../system/ai/tools/escalateConversation";
+import { resolveConversation } from "../system/ai/tools/resolveConversation";
 import { saveMessage } from "@convex-dev/agent";
 //import { search } from "../system/ai/tools/search";
 
@@ -51,22 +51,28 @@ export const create = action({
     }
 
     // TODO: Implement subscription check
-    // const shouldTriggerAgent =
-    //   conversation.status === "unresolved";
+    const shouldTriggerAgent =
+        conversation.status === "unresolved";
 
-    // if (shouldTriggerAgent) {
+    if (shouldTriggerAgent) {
       await supportAgent.generateText(
         ctx,
         { threadId: args.threadId },
         {
           prompt: args.prompt,
-          // tools: {
-          //   escalateConversationTool: escalateConversation,
-          //   resolveConversationTool: resolveConversation,
-          //   searchTool: search,
-          // }
+          tools: {
+            escalateConversationTool: escalateConversation,
+            resolveConversationTool: resolveConversation,
+            //searchTool: search,
+          }
         },
       )
+    } else {
+      await saveMessage(ctx, components.agent, {
+        threadId: args.threadId,
+        prompt: args.prompt,
+      });
+    }
   },
 });
 
